8b9e88c03bc683befb5b10300f19c077b27f3255
madhumita-chatne <madhumitachatne@gmail.com>
2021-10-28 15:26:29 +0530

    cors

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ const app = require("express");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://socket-backend-3towjekr2q-ue.a.run.app",
     methods: ["GET", "POST"],
   },
 });
