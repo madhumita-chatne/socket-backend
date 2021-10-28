1e7dfe4bcbe5f0ad43d1183ce02f0e6c77b2015c
madhumita-chatne <madhumitachatne@gmail.com>
2021-10-28 17:51:16 +0530

    removed disconnect bug

--- index.ts
+++ index.ts
@@ -1,30 +1,30 @@
+// 
 const app = require("express");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: ["https://socket-frontend-3towjekr2q-ue.a.run.app", "http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://socket-frontend-3towjekr2q-ue.a.run.app"],
     methods: ["GET", "POST"],
   },
 });
-
-const PORT = process.env.PORT || 4000;
 let arr: String[] = [];
 io.on("connection", (socket: any) => {
   console.log("New client connected", socket.id);
+  //
   arr.push(`${socket.id} is now connected! Total = ${io.engine.clientsCount}`);
   socket.emit("status", arr);
-  
   socket.on("disconnect", () => {
-    arr.push(`${socket.id} is now disconnected! total = ${io.engine.clientsCount}`);
-    socket.emit("status", arr);
-    console.log("Total : ", io.engine.clientsCount)
-    if(io.engine.clientsCount === 0){
-      arr = []
+    arr.push(`${socket.id} is now disconnected! Total = ${io.engine.clientsCount}`);
+    socket.broadcast.emit("status", arr);
+
+    if(io.engine.clientsCount === 0 ){
       console.log("Refreshing")
+      arr = []
     }
   });
 });
 
-http.listen(PORT, () => {
-  console.log("App running on 4000");
+const port = process.env.PORT!;
+http.listen(port, () => {
+  console.log("App running on ", port);
 });
