9a9355024c2e354faa519cdc01c1652e9cdabd79
madhumita-chatne <madhumitachatne@gmail.com>
2021-10-28 16:03:14 +0530

    Total clients seen, arr set to 0 when everyone disconnects

--- index.ts
+++ index.ts
@@ -2,21 +2,32 @@ const app = require("express");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "https://socket-frontend-3towjekr2q-ue.a.run.app",
+    origin: ["https://socket-frontend-3towjekr2q-ue.a.run.app", "http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
 
-const PORT = process.env.PORT || 8080;
-const arr: String[] = [];
+const PORT = process.env.PORT || 4000;
+let arr: String[] = [];
 io.on("connection", (socket: any) => {
   console.log("New client connected", socket.id);
-  //
-  arr.push(`${socket.id} is now connected`);
+  io.engine.clientsCount
+  arr.push(`${socket.id} is now connected! Total = ${io.engine.clientsCount}`);
   socket.broadcast.emit("status", arr);
+
+  socket.on("connect", ()=>{
+    socket.emit("status ", `Welcome  ${socket.id}`)
+  })
+
+  
   socket.on("disconnect", () => {
-    arr.push(`${socket.id} is now disconnected`);
+    arr.push(`${socket.id} is now disconnected! total = ${io.engine.clientsCount}`);
     socket.broadcast.emit("status", arr);
+    console.log("Total : ", io.engine.clientsCount)
+    if(io.engine.clientsCount === 0){
+      arr = []
+      console.log("Refreshing")
+    }
   });
 });
 
